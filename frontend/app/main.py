9ddd2372ac73bbf7002ceb478d7fb9ddeef87c0c
Vlad <Vladkovalov777@gmail.com>
2025-11-13 20:09:43 +0100

    frontend

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
-
+templates = Jinja2Templates(directory="templates")
 
 
 def get_application() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(debug=True)
 
+    @app.get("/")
+    async def index(request: Request):
+        context = {
+            "request": request,
+            "title": "Головна сторінка сайту"
+        }
+
+        response = templates.TemplateResponse('pages/index.html', context=context)
+
+        return response
 
 
     return app
