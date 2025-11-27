602865fc268de8c44b0f5c43061f5822d68ee0ee
Vlad <Vladkovalov777@gmail.com>
2025-11-27 20:00:31 +0100

    register form

--- frontend/app/router_user.py
+++ frontend/app/router_user.py
@@ -21,13 +21,15 @@ async def index(request: Request):
 
 
 @router_user.get("/register")
+@router_user.post("/register")
 async def user_register(request: Request):
+    print(request.method, 8888888888888)
     context = {
         "request": request,
         "title": "Головна сторінка сайту",
         "user": {}
     }
 
-    response = templates.TemplateResponse('pages/index.html', context=context)
+    response = templates.TemplateResponse('pages/register.html', context=context)
 
     return response
