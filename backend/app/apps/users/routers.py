88beec49ff8ea22a09a5a09eaef9c28892554c42
Vlad <Vladkovalov777@gmail.com>
2025-12-04 19:38:02 +0100

    register form 4

--- backend/app/apps/users/routers.py
+++ backend/app/apps/users/routers.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 
 from apps.auth.password_handler import PasswordHandler
 from apps.core.dependencies import get_async_session
 from apps.users.crud import user_manager
+from apps.users.models import User
 from apps.users.schemas import SavedUserSchema, RegisterUserSchema
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -15,8 +16,12 @@ async def create_user(
         session: AsyncSession = Depends(get_async_session)
 ) -> SavedUserSchema:
     """create user based on name email password"""
+    maybe_user = await user_manager.get(session=session, model_field=User.email, value=user_data.email)
+    if maybe_user:
+        raise HTTPException(detail=f'User with email {user_data.email} already exists',
+                            status_code=status.HTTP_409_CONFLICT)
+
     hashed_password = await PasswordHandler.get_password_hash(user_data.password)
-    # is_valid = await PasswordHandler.verify_password(user_data.password, hashed_password)
     saved_user = await user_manager.create(
         session=session,
         name=user_data.name,
--- frontend/app/router_user.py
+++ frontend/app/router_user.py
@@ -28,24 +28,16 @@ async def user_register(request: Request, email: str = Form(''), username: str =
     context = {
         "request": request,
         "title": "Register",
-        "user": {}
+        "user": {},
+        "email": email,
+        "username": username,
+        "error": ""
     }
     if request.method == "GET":
 
         response = templates.TemplateResponse('pages/register.html', context=context)
         return response
 
-    """
-    curl -X 'POST' \
-      'http://localhost:19999/users/create' \
-      -H 'accept: application/json' \
-      -H 'Content-Type: application/json' \
-      -d '{
-      "email": "user@example.com",
-      "name": "string",
-      "password": "string"
-    }'
-    """
     async with httpx.AsyncClient() as client:
         headers = {
             "Content-Type": "application/json",
@@ -57,7 +49,10 @@ async def user_register(request: Request, email: str = Form(''), username: str =
         }
         response = await client.post("http://backend:20001/users/create", json=payload, headers=headers)
 
-
+        if response.status_code == status.HTTP_409_CONFLICT:
+            context['error'] = f"Користувач {email} вже зареєстрований"
+            response = templates.TemplateResponse('pages/register.html', context=context)
+            return response
 
     response = RedirectResponse(request.url_for('index'), status_code=status.HTTP_303_SEE_OTHER)
     return response
