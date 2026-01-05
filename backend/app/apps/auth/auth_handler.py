d847027913c3f12f00ab8a941a497c8dbad9fd3c
Vlad <Vladkovalov777@gmail.com>
2026-01-05 17:09:43 +0100

    refresh token

--- backend/app/apps/auth/auth_handler.py
+++ backend/app/apps/auth/auth_handler.py
@@ -1,4 +1,6 @@
 from datetime import timedelta, datetime
+from uuid import uuid4
+
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,6 +8,7 @@ import jwt
 
 from apps.users.crud import user_manager
 from apps.users.models import User
+from services.redis_service import redis_service
 from settings import settings
 
 
@@ -18,10 +21,13 @@ class AuthHandler:
         access_token_payload = {
             'sub': user.email
         }
+
+        refresh_key = uuid4().hex
         refresh_token_payload = {
             'sub': user.email,
+            'key': refresh_key
         }
-        # todo process refresh token
+        redis_service.set_key(F"refresh_token_key:{refresh_key}", '1', settings.REFRESH_TOKEN_LIFETIME_MINUTES * 60)
 
         return {
             "access_token": await self.generate_token(access_token_payload,
@@ -54,12 +60,22 @@ class AuthHandler:
             raise HTTPException(detail=f'token expired',
                                 status_code=status.HTTP_401_UNAUTHORIZED)
         except jwt.InvalidTokenError as e:
-            print(e, 888888888888888888)
             raise HTTPException(detail=f'where have you got this token, dude',
                                 status_code=status.HTTP_401_UNAUTHORIZED)
 
     async def get_token_pairs_by_refresh_token(self, refresh_token: str, session: AsyncSession) -> dict:
         payload = await self.decode_token(refresh_token)
+        refresh_token_key = payload.get("key")
+        if not refresh_token_key:
+            raise HTTPException(detail=f'Invalid refresh', status_code=status.HTTP_401_UNAUTHORIZED)
+
+        refresh_token_key_in_redis = redis_service.get(F"refresh_token_key:{refresh_token_key}")
+        if not refresh_token_key_in_redis:
+            raise HTTPException(detail=f'Redis token was used already',
+                                status_code=status.HTTP_401_UNAUTHORIZED)
+
+        redis_service.delete(F"refresh_token_key:{refresh_token_key}")
+
         user: User = await user_manager.get(session=session, model_field=User.email, value=payload['sub'])
         if not user:
             raise HTTPException(detail=f'User with email {payload["sub"]} not found',
